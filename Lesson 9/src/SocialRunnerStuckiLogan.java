d85e0e4b3ee597907cb42900fa1ec921591f5d2c
Stucki <stuckil@bhs1200maclab13.bentonville.k12.ar.us>
2016-11-03 13:40:55 -0500

    ugh

---
+++ Lesson 9/src/SocialRunnerStuckiLogan.java
@@ -0,0 +1,14 @@
+//A+ Computer Science  -  www.apluscompsci.com
+//Name - Logan Stucki
+//Date - 3/11/16
+//Class - 6th period
+//Lab  - Social Security
+
+import static java.lang.System.*;
+
+public class SocialRunnerStuckiLogan {
+	public static void main(String args[]) {
+		// add test cases
+
+	}
+}
---
+++ Lesson 9/src/SocialStuckiLogan.java
@@ -0,0 +1,36 @@
+//A+ Computer Science  -  www.apluscompsci.com
+//Name - Logan Stucki
+//Date - 3/11/16
+//Class - 6th period
+//Lab  - Social Security
+
+import static java.lang.System.*;
+
+public class SocialStuckiLogan {
+   private String socialNum;
+   private int sum;
+
+	public SocialStuckiLogan() {
+		this("");
+	}
+
+	public SocialStuckiLogan(String soc) {
+		setWord(soc);
+	}
+
+
+	public void setWord(String w) {
+		socialNum = w;
+		sum = 0;
+	}
+
+	public void chopAndAdd() {
+		int firstDash = socialNum.indexOf('-');
+		String sub = socialNum.substring(0, firstDash);
+		
+	}
+
+	public String toString() {
+		return "SS# " + socialNum + " has a total of " + sum + "\n";
+	}
+}
