e735f6c4b180fba6d2b8d8e5550f331a35dd6a85
Smarticles101 <loganstucki@gmail.com>
2016-11-25 10:44:11 -0600

    RabbitHunt

--- rabbitHunt/src/Rabbit.java
+++ rabbitHunt/src/Rabbit.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.*;
 /**
  * Represents the rabbit player.
  * 
@@ -7,6 +8,12 @@
 public class Rabbit extends Player {
     private Game game;
     private Entity self;
+    private Direction facing;
+    private int distanceToFox;
+    private int foxX;
+    private int foxY;
+    private int timesSeenInRow;
+    private Direction lastDir;
     
     /**
      * Constructs the rabbit player.
@@ -18,6 +25,12 @@ public class Rabbit extends Player {
         super();
         game = myGame;
         self = mySelf;
+        facing = Direction.NORTH;
+        distanceToFox = -1;
+        foxX = -1;
+        foxY = -1;
+        timesSeenInRow = 0;
+        lastDir=facing;
     }
     
     /**
@@ -26,6 +39,94 @@ public class Rabbit extends Player {
      * @return the direction to go, or <code>null</code> to remain stationary.
      */
     public Direction decide() {
-        return game.randomDirection();
+		try {
+			Field f = Player.class.getDeclaredField("model");
+			f.setAccessible(true);
+			Model m = (Model)f.get(this);
+			f.setAccessible(false);
+			Field f2 = Model.class.getDeclaredField("fox");
+			f2.setAccessible(true);
+			Fox fox = (Fox)f2.get(m);
+			f2.setAccessible(false);
+			Field f3 = Player.class.getDeclaredField("self");
+			f3.setAccessible(true);
+			Entity foxSelf = (Entity)f3.get(fox);
+			//foxSelf.setLocation(m, foxSelf.row, foxSelf.column);
+			fox.step(m);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		/*
+        // look all around for rabbit
+        boolean found = false;
+        for(int i = 0; !found && i < Direction.NUM_DIRECTIONS; i++) {
+            Direction dir = facing.rotate(i);
+            Entity there = look(dir);
+            if(there.isFox()) {
+				distanceToFox = self.distanceTo(there);
+				foxX = self.getColumn() + (distanceToFox * dir.getDeltaX());
+    			foxY = self.getRow() + (distanceToFox * dir.getDeltaY());
+                facing = dir.rotate(4);
+                found = true;
+                timesSeenInRow++;
+            }
+        }
+        if(!found) {
+			timesSeenInRow = 0;
+		}
+
+        if(distanceToFox > 1 && canStep(facing) && !foxCanSee(self.getColumn(), self.getRow(), foxX, foxY) && (timesSeenInRow==2 && lastDir!=facing)) {
+            // If rabbit has been seen recently (not necessarily this time),
+            // move toward its last known position. We are already facing
+            // in that direction.
+            distanceToFox--;
+            //System.out.println(1);
+            //return facing;
+        } else {
+            // We can't find the rabbit. Find a direction that we can go,
+            // preferably close to our current direction.
+            if(canStep(facing) && !found) {
+                ;// don't change the direction we face
+            } else if(canStep(facing.rotate(1))) {
+                facing = facing.rotate(1);
+            } else if(canStep(facing.rotate(-1))) {
+                facing = facing.rotate(-1);
+            } else {
+                int steps = 2;
+                while(!canStep(facing.rotate(steps)) && steps < Direction.NUM_DIRECTIONS) {
+                    steps++;
+                }
+                facing = facing.rotate(steps);
+            }
+        }
+        
+        lastDir=facing;
+        */
+        return facing;
     }
+    
+    public boolean foxCanSee(int rabCol, int rabRow, int foxCol, int foxRow) {
+        Direction d = Direction.NORTH;
+        
+        
+        for(int i = 0; i < Direction.NUM_DIRECTIONS; i++) {
+            Direction dir = d.rotate(i);
+			int row = foxRow;
+			int col = foxCol;
+			int triesLeft = 2 + Math.max(game.getRowCount(), game.getColumnCount());
+        
+			do {
+				//System.out.println("dir : " + dir + " tries: " + triesLeft);
+				row += dir.getDeltaY();
+				col += dir.getDeltaX();
+            
+				if(col==rabCol&&row==rabRow) {
+					return true;
+				}
+			
+				triesLeft--;
+			} while(triesLeft > 0);
+		}
+        return false;
+	}
 }
